a7f3f3214cc6530a1c0b0c02a1406d8349be0484
Shaun <seansama@pop-os.localdomain>
2023-01-16 09:43:51 +0300

    Creating user component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Home from "../Views/Home";
 
 function Navbar(){
-
+const links = ["Home", "About"]
+    return(
+        <a href="#Home"></a>
+    )
 }
 
 export default Navbar;
---
+++ src/Components/User.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+
+class User extends Component{
+    render() {
+        return(
+            <div style={{
+                background: "white",
+                padding: 50px
+            }}>
+
+            </div>
+            )
+    }
+}
+
+
+
+
+
+
+/*const User = () => {
+  return(
+      <div>
+
+      </div>
+  )
+}
+
+ */
